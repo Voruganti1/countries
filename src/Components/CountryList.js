08c4542b6c3d6387a142190ac885dd9c803a549f
Voruganti1 <sudhatvs@yahoo.com>
2022-09-14 17:55:15 +0100

    many changes

--- src/Components/CountryList.js
+++ src/Components/CountryList.js
@@ -1,21 +1,69 @@
-import React from "react";
-import AmericaStates from "./AmericaStates";
-const CountryList = () => {
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const AmericaStates = () => {
+  const [countries, setCountries] = useState(null);
+  const [t, setToken] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  async function getData() {
+    const token = await axios.get(
+      "https://www.universal-tutorial.com/api/getaccesstoken",
+      {
+        headers: {
+          Accept: "application/json",
+          "api-token":
+            "gqBd7hHdap_xivuuXxv6gg-NjnPtTTtHJI0CIVOB2Ora0N-0HYv0jiFsPo_G4d8pp1Y",
+          "user-email": "test@testccc.com",
+        },
+      }
+    );
+    //Not working
+    setToken(token.data.auth_token);
+
+    const countriesList = await axios.get(
+      "https://www.universal-tutorial.com/api/countries/",
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + token.data.auth_token,
+        },
+      }
+    );
+    /*const stateList = await axios.get(
+      " https://www.universal-tutorial.com/api/states/",
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + token.data.auth_token,
+        },
+      }
+    );*/
+
+    setCountries(countriesList.data);
+    //console.log(selectedCountry);
+    // console.log(countriesList);
+  }
+  useEffect(() => {
+    if (!countries) {
+      getData();
+    }
+  }, []);
+  const countrySelectHandler = (event) => {
+    console.log(event);
+  };
+
   return (
     <div>
-      <form action="">
-        <label htmlFor="countries">Select a country</label>
-        <select name="country" id="">
-          <option value="india">India</option>
-          <option value="UK">UK</option>
-          <option value="America">America</option>
-          <option value="Japan">Japan</option>
-          <option value="China">China</option>
-          <option value="Canada">Canada</option>
-        </select>
-      </form>
-      <AmericaStates />
+      <select onChange={countrySelectHandler()}>
+        <option>---select---</option>
+        {countries &&
+          countries.map((c, i) => (
+            <option key={i} value={c.country_short_name}>
+              {c.country_name}
+            </option>
+          ))}
+      </select>
     </div>
   );
 };
-export default CountryList;
+export default AmericaStates;
